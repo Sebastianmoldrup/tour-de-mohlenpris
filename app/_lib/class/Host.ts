6184d4d8cb85791259ee65d0958099fd8127f6dc
Sebastian Home <sebastianmoldrup@Sebastians-Air.home>
2025-03-18 01:04:01 +0100

    restructuring

--- app/_lib/class/Host.ts
+++ app/_lib/class/Host.ts
@@ -1,92 +1,25 @@
-import { HostJsonType, GuestType } from "@/app/_lib/types";
-
-export class Host {
+interface GuestType {
+  allergy: string | null;
+  co_guest: string | null;
+  id: number;
+  last_name: string;
   name: string;
-  seats: number;
-  menu: {
-    appetizer: string;
-    dinner: string;
-    dessert: string;
-  };
-  dietary: {
-    allergy: {
-      app: string | null;
-      dinner: string | null;
-      dessert: string | null;
-    };
-    veg: {
-      app: boolean;
-      dinner: boolean;
-      dessert: boolean;
-    };
-  };
-  guests: {
-    appetizer: string[];
-    dinner: string[];
-    dessert: string[];
-  };
+  vegeterian: string | null;
+}
 
-  constructor(host: HostJsonType) {
-    this.name = host.name;
-    this.seats = host.seats;
-
-    // Host menu
-    this.menu = {
-      appetizer: host.appetizer,
-      dinner: host.dinner,
-      dessert: host.dessert,
-    };
-
-    // Host meal allergy & vegeterian
-    this.dietary = {
-      allergy: {
-        app: host.appetizer_allergy ?? null,
-        dinner: host.dinner_allergy ?? null,
-        dessert: host.dessert_allergy ?? null,
-      },
-      veg: {
-        // Removing whitespace and looking for key "vegetar"
-        // in allergy to give truthy statement for vegeterian meal
-        app: (host.appetizer_allergy || "")
-          .split(",")
-          .map((x) => x.trim())
-          .includes("vegetar"),
-        dinner: (host.dinner_allergy || "")
-          .split(",")
-          .map((x) => x.trim())
-          .includes("vegetar"),
-        dessert: (host.dessert_allergy || "")
-          .split(",")
-          .map((x) => x.trim())
-          .includes("vegetar"),
-      },
-    };
-
-    // Host meal guests
-    this.guests = {
-      appetizer: [],
-      dinner: [],
-      dessert: [],
-    };
-  }
-
-  isVegeterian() {
-    return Object.values(this.dietary).filter((meal) => meal);
-  }
-
-  getSeatings() {
-    return this.seats;
-  }
-
-  addAppetizerGuest(guest: GuestType) {
-    this.guests.appetizer.push(guest.name);
-  }
-
-  addDinnerGuest(guest: GuestType) {
-    this.guests.dinner.push(guest.name);
-  }
-
-  addDessertGuest(guest: GuestType) {
-    this.guests.dessert.push(guest.name);
+export class Guest {
+  #id: number;
+  #name: string;
+  #last_name: string;
+  #allergy: string | null;
+  #vegeterian: string | null;
+  #co_guest: string | null;
+  constructor(guest: GuestType) {
+    this.#id = guest.id;
+    this.#name = guest.name;
+    this.#last_name = guest.last_name;
+    this.#allergy = guest.allergy;
+    this.#vegeterian = guest.vegeterian;
+    this.#co_guest = guest.co_guest;
   }
 }
