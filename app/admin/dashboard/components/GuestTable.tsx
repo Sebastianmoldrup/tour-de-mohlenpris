f5d8718ebd1c3e2ddb0bd7b17965ec7626aa452d
Sebastian Home <sebastian.moldrup@gmail.com>
2025-06-29 23:10:36 +0200

    chore: trigger redeploy

--- app/admin/dashboard/components/GuestTable.tsx
+++ app/admin/dashboard/components/GuestTable.tsx
@@ -156,13 +156,16 @@ export default function GuestTable({
 
                 {/* Guest vegeterian */}
                 <TableCell>
-                  {guest.vegeterian ? <Leaf className="w-5 text-green-500" /> : null}
+                  {guest.vegeterian ? (
+                    <Leaf className="w-5 text-green-500" />
+                  ) : null}
                 </TableCell>
-
 
                 {/* Guest's meals */}
                 {mealTypes.map((type) => {
-                  const meal = guest.meals.find((meal: Meal) => meal.type === type);
+                  const meal = guest.meals.find(
+                    (meal: Meal) => meal.type === type,
+                  );
 
                   return (
                     <TableCell key={type} className="capitalize space-y-2">
@@ -196,7 +199,10 @@ export default function GuestTable({
                             }}
                           >
                             <SelectTrigger className="w-[180px] md:w-fit">
-                              <SelectValue className="font-semibold" placeholder={meal.host.name} />
+                              <SelectValue
+                                className="font-semibold"
+                                placeholder={meal.host.name}
+                              />
                             </SelectTrigger>
                             <SelectContent>
                               {allMeals
@@ -218,9 +224,13 @@ export default function GuestTable({
 
                           {/* Allergies */}
                           {meal.allergies.length > 0 ? (
-                            <div className="text-red-500">{meal.allergies.join(", ")}</div>
+                            <div className="text-red-500">
+                              {meal.allergies.join(", ")}
+                            </div>
                           ) : (
-                            <div className="text-green-500">Ingen allergier</div>
+                            <div className="text-green-500">
+                              Ingen allergier
+                            </div>
                           )}
                         </>
                       ) : (
@@ -236,7 +246,6 @@ export default function GuestTable({
       </Table>
     </div>
   );
-
 
   // return (
   //   <div className="lg:block overscroll-x-scroll overflow-hidden my-6">
